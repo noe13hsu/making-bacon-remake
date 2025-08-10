2937674e12549ed797236a5c0cb40b8d83c92724
Noe Hsu <noe13hsu@gmail.com>
2025-08-10 23:34:51 +1000

    Fix indentation

--- src/hooks/useUserQuery.ts
+++ src/hooks/useUserQuery.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query"
 import { getUser } from "../api/user"
 
 export const useUserQuery = () => {
-    return useQuery({
+  return useQuery({
     queryKey: ["user"],
     queryFn: getUser,
     retry: false,
