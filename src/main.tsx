4944bbf0f8122a3f152a120aa0b74cb622c8719e
Noe Hsu <noe13hsu@gmail.com>
2025-08-10 19:06:15 +1000

    Add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BrowserRouter } from "react-router-dom"
 
-import './index.css'
-import App from './App.tsx'
+import "./index.css"
+import App from "./App.tsx"
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </QueryClientProvider>
 )
