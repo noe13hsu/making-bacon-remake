f8dee30fef3e580cfe27618b7b081596145ca1a0
Noe Hsu <noe13hsu@gmail.com>
2025-08-10 19:11:18 +1000

    Add useUserQuery

---
+++ src/hooks/useUserQuery.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query"
+
+import { getUser } from "../api/user"
+
+export const useUserQuery = () => {
+    return useQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+    retry: false,
+  })
+}
